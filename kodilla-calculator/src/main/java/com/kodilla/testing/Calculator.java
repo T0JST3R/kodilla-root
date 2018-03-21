9cc01ad3afb9b83d34c9cae5445477cdb7f5d352
Tojsterr15 <bartosz.binkowski11@gmail.com>
2018-03-21 16:21:00 +0100

    Kodilla-addAtoBCalculator

---
+++ kodilla-calculator/src/main/java/com/kodilla/testing/Calculator.java
@@ -0,0 +1,12 @@
+package com.kodilla.testing;
+
+public class Calculator {
+
+    double addAtoB(double a, double b){
+        return a+b;
+    }
+
+    double substractAfromB (double a , double b){
+        return a-b;
+    }
+}
