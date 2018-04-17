b187b0897dd56f369f97d725ee80e7eb41b6e056
Tojsterr15 <bartosz.binkowski11@gmail.com>
2018-04-17 11:25:59 +0200

    PoemBeautifier

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -7,7 +7,11 @@ import com.kodilla.stream.book.BookDirectory;
 import com.kodilla.stream.com.kodilla.stream.forumuser.Forum;
 import com.kodilla.stream.com.kodilla.stream.forumuser.ForumUser;
 import com.kodilla.stream.com.kodilla.stream.person.People;
-
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.beautifier.PoemDecorator;
+import com.kodilla.stream.iterate.NumbersGenerator;
+import com.kodilla.stream.lambda.Executor;
+import com.kodilla.stream.lambda.Processor;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
@@ -30,6 +34,24 @@ public class StreamMain {
                 .forEach(System.out::println);
 
 
+
+        //POEMBEAUTIFIER
+
+
+        Processor processor = new Processor();
+        Executor codeToExecute = () -> System.out.println("This is an example text.");
+        processor.execute(codeToExecute);
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        PoemDecorator decorator = () -> System.out.println("text".toUpperCase());
+        PoemDecorator decorator1 = () -> System.out.println("DwaRazyDwa".toLowerCase());
+        PoemDecorator decorator2 = () -> System.out.println("      CzyPiencDwa     ".trim());
+        PoemDecorator decorator3 = () -> System.out.println("Czy plus dwa = czterdziesci".substring(0 , 20) + "y");
+        poemBeautifier.beautifier(decorator);
+        poemBeautifier.beautifier(decorator1);
+        poemBeautifier.beautifier(decorator2);
+        poemBeautifier.beautifier(decorator3);
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
 
 
 
