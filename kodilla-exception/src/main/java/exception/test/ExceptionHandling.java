e01e8e7a69296e7b44c39f1f682fe69f8012533c
Tojsterr15 <bartosz.binkowski11@gmail.com>
2018-06-13 19:27:52 +0200

    Exceptions test

--- kodilla-exception/src/main/java/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/exception/test/ExceptionHandling.java
@@ -1,15 +1,22 @@
 package exception.test;
 
-public class ExceptionHandling {
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ExceptionHandling {
+    private static final Logger LOGGER = Logger.getLogger(ExceptionHandling.class.toString());
     public static void main(String[] args) {
 
         SecondChallenge secondChallenge = new SecondChallenge();
 
         try {
-            secondChallenge.probablyIWillThrowException(3, 4);
+            secondChallenge.probablyIWillThrowException(3, 3);
+          //  secondChallenge.probablyIWillThrowException(3, 4);
+
         } catch (Exception e) {
-            System.out.println("Exception!");
+            System.out.println("Exception!" + e.getMessage());
+            LOGGER.log(Level.SEVERE , "Exception" , e);
         } finally {
             System.out.println("Program finished work");
         }
--- kodilla-exception/src/main/java/exception/test/SecondChallenge.java
+++ kodilla-exception/src/main/java/exception/test/SecondChallenge.java
@@ -4,7 +4,7 @@ public class SecondChallenge {
 
     public String probablyIWillThrowException(double x, double y) throws Exception {
         if(x >= 2 || x < 1 || y == 1.5) {
-            throw new Exception();
+            throw new Exception("Error!");
         }
         return "Done!";
     }
