956b22a00789adb42fcc9821d9e1ec5fab3451cf
Tojsterr15 <bartosz.binkowski11@gmail.com>
2018-08-18 22:26:33 +0200

    Selenium Facebook tets

--- kodilla_testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
+++ kodilla_testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -7,11 +7,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
 public class FacebookTestingApp {
-    public final static  String XPATH_FIRSTNAME = "//div[contains(@class , \"mbm _1iy_ _a4y _3-90 lfloat _ohe\")]/div/div/input";
-    public final static  String XPATH_LASTNAME = "//div[contains(@class , \"mbm _1iy_ _a4y rfloat _ohf\")]/div/div/input";
+    public final static  String XPATH_FIRSTNAME = "//input[contains(@name , \"firstname\")]";
+    public final static  String XPATH_LASTNAME = "//input[contains(@name , \"lastname\")]";
+    public final static  String XPATH_PASSWORD = "//input[contains(@name , \"reg_passwd__\")]";
     public final static  String XPATH_WAITFOR = "//div[contains(@class , \"fb_content clearfix \")]";
-    public final static  String XPATH_MAILORPHONENUM = "//input[contains(@id , \"u_0_q\")]";
-    public final static  String XPATH_SELECTORS = "//span[contains(@class , \"_5k_4\")]/span";
+    public final static  String XPATH_MAILORPHONENUM = "//input[contains(@name , \"reg_email__\")]";
+    public final static  String XPATH_SELECTORS = "//span[contains(@data-name , \"birthday_wrapper\")]/span";
 
 
     public static void main(String[] args) {
@@ -23,6 +24,7 @@ public class FacebookTestingApp {
         WebElement firstname = driver.findElement(By.xpath(XPATH_FIRSTNAME));
         WebElement lastname = driver.findElement(By.xpath(XPATH_LASTNAME));
         WebElement mailorphonenum = driver.findElement(By.xpath(XPATH_MAILORPHONENUM));
+        WebElement passwordElement = driver.findElement(By.xpath(XPATH_PASSWORD));
         WebElement selector1 = driver.findElement(By.xpath(XPATH_SELECTORS + "/select[1]"));
         WebElement selector2 = driver.findElement(By.xpath(XPATH_SELECTORS + "/select[2]"));
         WebElement selector3 = driver.findElement(By.xpath(XPATH_SELECTORS + "/select[3]"));
@@ -32,6 +34,7 @@ public class FacebookTestingApp {
         firstname.sendKeys("TEST");
         lastname.sendKeys("TEST");
         mailorphonenum.sendKeys("TEST@MAIL.COM");
+        passwordElement.sendKeys("PASSWORD");
         selectorr1.selectByIndex(3);
         selectorr2.selectByIndex(12);
         selectorr3.selectByIndex(44);
